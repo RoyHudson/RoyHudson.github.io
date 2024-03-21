a0a1d21eef12530452fc03dc1b951ae9fe0cc33f
RoySykes <royhudson2099@gmail.com>
2024-03-20 22:20:43 -0500

    Tailwind install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://RoyHudson.github.io',
   base: '/my-repo',
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
